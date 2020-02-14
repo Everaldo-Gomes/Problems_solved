f40c24a4bb54aec965b217f18a07de7f73286f8f
Everaldo Gomes <everaldo332@gmail.com>
2020-02-14 07:09:06 -0300

    Add files via upload

---
+++ Uri_online_judge/1 - Beginner/C++/1044 Multiples.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main(){
+
+    int a, b;
+    cin >> a >> b;
+
+    a = abs(a);
+    b = abs(b);
+
+    if(b > a) swap(a,b);
+    if(a % b == 0) cout << "Sao Multiplos\n";
+    else cout << "Nao sao Multiplos\n";
+
+    return 0;
+}
