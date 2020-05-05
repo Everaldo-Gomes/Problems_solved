6fc05733d1d636b0173298b2d1d04af60010e98a
Everaldo-Gomes <everaldo332@gmail.com>
2020-05-05 18:32:30 -0300

    upload

---
+++ Code_forces/ProblemsSet/61A_ultra_fast_mathematician.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+  string s1, s2;
+  cin >> s1 >> s2;
+
+  for(unsigned int i = 0; i < s1.size(); i++) {
+    if(s1[i] == s2[i]) s1[i] = '0';
+    else {
+      s1[i] = '1';
+    }
+  }
+
+  for(unsigned int i = 0; i < s1.size(); i++) { cout << s1[i]; }
+
+  cout << endl;
+  return 0;
+}
