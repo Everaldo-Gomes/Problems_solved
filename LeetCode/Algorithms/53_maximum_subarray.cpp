e4e08a58b09e0d7dd3b5dfb36727bea7ef753d31
Everaldo-Gomes <everaldo332@gmail.com>
2022-01-23 05:43:23 -0300

    upload

---
+++ LeetCode/Algorithms/53_maximum_subarray.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        
+        int local_max = 0;
+        int global_max = -999999;
+        
+        for (int i = 0; i < nums.size(); i++) {
+            
+            local_max = max(nums[i], nums[i] + local_max);
+            
+            if (local_max > global_max) {
+                global_max = local_max;
+            }
+        }
+        
+        return global_max;
+    }
+};
