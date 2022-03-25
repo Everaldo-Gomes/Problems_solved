2327bfcd873a6fe1515dead1d61dddc5df5ee273
Everaldo-Gomes <everaldo332@gmail.com>
2022-03-25 07:14:56 -0300

    upload

---
+++ Uri_online_judge/1 - Beginner/C++/2756_output10.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main () {
+
+	cout << "       A\n"
+		 << "      B B\n"
+		 << "     C   C\n"
+		 << "    D     D\n"
+		 << "   E       E\n"
+		 << "    D     D\n"
+		 << "     C   C\n"
+		 << "      B B\n"
+		 << "       A\n";
+	return 0;
+}
