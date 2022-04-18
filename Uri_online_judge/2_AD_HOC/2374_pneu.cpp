1f0df24ae10a58a10caf15704945012ed9ebf741
Everaldo-Gomes <everaldo332@gmail.com>
2022-04-18 07:20:06 -0300

    upload

---
+++ Uri_online_judge/2_AD_HOC/2374_pneu.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main () {
+
+	int desired_pressure, read_pressure;
+	cin >> desired_pressure >> read_pressure;
+
+	cout << desired_pressure - read_pressure << endl;	
+		
+	return 0;
+}
