57e4139d88de17ad6aa85626e57f9abf986c9731
Everaldo-Gomes <everaldo332@gmail.com>
2022-05-13 07:41:26 -0300

    upload

---
+++ Uri_online_judge/1 - Beginner/C++/1924 Vitória and her indecision.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+    int QntCourse;
+    char CourseName[100];
+
+    cin >> QntCourse;
+
+    for(int i = 0; i < QntCourse; i++)
+    {
+        cin >> CourseName;
+    }
+    cout << "Ciencia da Computacao" << endl;
+
+    return 0;
+}
