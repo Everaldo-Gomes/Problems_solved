c5f7b7243e0bf3d3723960e3e4727f746538544d
Everaldo-Gomes <everaldo332@gmail.com>
2022-09-28 07:25:04 -0300

    upload

---
+++ Uri_online_judge/1 - Beginner/C++/2172_event.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main ()
+{
+   
+	while (1)
+	{
+		long long int x, m, xp;
+		cin >> x >> m;
+		
+		if (x == 0)
+			break;
+		else
+			xp = x * m;
+	
+		cout << xp << "\n";		
+	}
+
+	
+	return 0;
+}
