42eb5d377c174af4cd6ce7c625e0ddac18752353
Everaldo-Gomes <everaldo332@gmail.com>
2022-10-02 09:23:04 -0300

    upload

---
+++ Uri_online_judge/1 - Beginner/C++/2581_i_am_toorg.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main ()
+{
+	int n;
+	cin >> n;
+
+	while (n--)
+	{
+		string str;
+		cin >> str;
+		
+		cout << "I am Toorg!\n"; 
+	}
+	
+	return 0;
+}
