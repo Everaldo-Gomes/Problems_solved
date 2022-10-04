0e74a89ca8e39a3f07820d9c0a19ae4914bee7cd
Everaldo-Gomes <everaldo332@gmail.com>
2022-10-04 07:03:46 -0300

    upload

---
+++ Uri_online_judge/1 - Beginner/C++/2752_output_6.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main ()
+{
+
+	cout 	
+		<< "<AMO FAZER EXERCICIO NO URI>\n"
+		<< "<    AMO FAZER EXERCICIO NO URI>\n"
+		<< "<AMO FAZER EXERCICIO >\n"
+		<< "<AMO FAZER EXERCICIO NO URI>\n"
+		<< "<AMO FAZER EXERCICIO NO URI    >\n"
+		<< "<AMO FAZER EXERCICIO NO URI>\n"
+		<< "<          AMO FAZER EXERCICIO >\n"
+		<< "<AMO FAZER EXERCICIO           >\n";
+	
+	return 0;
+}
