06f69b53c3a5684cbc8a8388985f0b660ddc5910
Everaldo-Gomes <everaldo332@gmail.com>
2022-10-05 07:44:38 -0300

    upload

---
+++ Uri_online_judge/1 - Beginner/C++/2753_output.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main ()
+{
+	char c = 'a';
+	for (int i = 97; i <= 122; i++, c++)
+	{
+		cout << i << " e " << c << "\n"; 
+	}
+	
+	return 0;
+}
